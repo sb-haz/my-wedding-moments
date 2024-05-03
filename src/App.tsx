612722d6c07bb0573a04a4bcdb68e50210e58a01
sb-haz <sb_haz@icloud.com>
2024-05-03 20:37:07 +0100

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,15 @@ import './App.css'
 function App() {
   const instagram = "https://www.instagram.com/my.wedding.moments";
   const tiktok = "https://www.tiktok.com/@myweddingmoments";
+  const app = "www.myweddingmoments.com/"
 
   return (
     <div className="content">
       <div className="text-box">
         <h1><span>💍</span> My Wedding Moments</h1>
         <div className="links">
+          <a href={app} target="_blank">Get App</a>
+          <p>•</p>
           <a href={instagram} target="_blank">Instagram</a>
           <p>•</p>
           <a href={tiktok} target="_blank">TikTok</a>
